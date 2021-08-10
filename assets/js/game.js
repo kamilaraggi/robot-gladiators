c82b2c0fdbf1b67548c8a1c845e899c1bec29c7c
Kamila Raggi <kamilaraggi@hotmail.com>
2021-08-09 22:51:04 -0400

    add variables

---
+++ assets/js/game.js
@@ -0,0 +1,17 @@
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+  
+  fight();
